5254bebebacc54701b340abe6c8afa94d7aeded1
Philipelima <philipematheus.lim@gmail.com>
2022-06-27 02:28:03 -0300

    feat: create a Diretory class

---
+++ src/File/Directory.php
@@ -0,0 +1,25 @@
+<?php
+namespace Logger\File;
+
+class Directory 
+{
+    public static function createLogDirectory(): bool
+    {
+        
+        if(is_dir(LOGGER_PATH)) {
+           
+            $logPath = LOGGER_PATH."/log";
+ 
+            if(is_dir($logPath)) return true;
+
+
+            if(!is_dir($logPath)){
+                 mkdir($logPath);
+                 return true;
+            }
+
+            return false;
+        }
+        return false;
+    }
+}
