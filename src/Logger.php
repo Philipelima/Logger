825192f61334d04ddfde4de250e511776c60972a
Philipelima <philipematheus.lim@gmail.com>
2022-06-27 02:38:53 -0300

    refector: refectoring  logger class

--- src/Logger.php
+++ src/Logger.php
@@ -1,6 +1,9 @@
 <?php 
 namespace Logger;
 
+use Logger\File\File;
+use Logger\Report\Report;
+
 Class Logger 
 {
 
@@ -10,7 +13,7 @@ Class Logger
 
     protected static string $path;
 
-    private static $acceptedLevels = ['warning', 'info', 'success', 'error'];
+    private static $acceptedLevels = ['WARNING', 'INFO', 'SUCCESS', 'ERROR'];
 
 
     public function __toString()
@@ -19,16 +22,45 @@ Class Logger
     }
 
 
-
-    
     protected static function startLogger()
     {
         echo PHP_EOL;
-        echo "\e[0;31;42m Yeah! Logger  0.1.0 is running on this project! \e[0m\n";
+        echo "\e[0;31;42m Yeah! Logger is running on this project!                                           \e[0m\n";
         echo PHP_EOL;
     }
 
     
+
+    public static function info(string $message): void
+    {
+        self::setLevel('info');
+        self::saveLogMessage($message);
+    }
+
+
+
+    public static function warn(string $message): void
+    {
+        self::setLevel('warning');
+        self::saveLogMessage($message);
+    }
+
+    
+
+    public static function success(string $message): void
+    {
+        self::setLevel('success');
+        self::saveLogMessage($message);
+    }
+
+
+    public static function error($message): void
+    {
+        self::setLevel('error');
+        self::saveLogMessage($message);
+    }
+    
+
     private static function setLevel(string $level): void
     {
         $level          = strtoupper($level);
@@ -37,6 +69,18 @@ Class Logger
         if (in_array($level, $acceptedLevels)) {
             self::$level = trim($level);
         }
+    }
+
+
+    private static function saveLogMessage(string $message)
+    {
+        $date = date('Y-m-d');
+        $hour = date('H:m:i');
+
+        $level   = self::$level;
+        $message = "[${date}][${hour}][${level}] $message".PHP_EOL;
+
+        Report::save($message);
     }
     
 }
