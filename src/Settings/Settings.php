4291a65d3da2de57f2500484847bf5e26cd3fdc9
Philipelima <philipematheus.lim@gmail.com>
2022-06-27 02:18:17 -0300

    refector: Refectoring the configuration class

---
+++ src/Settings/Settings.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Logger\Settings;
+
+use Logger\Logger;
+use Logger\File\File;
+use Exception;
+use Logger\Exceptions\LoggerException;
+use LogicException;
+
+class Settings extends Logger
+{
+
+    
+
+    public static function settings(array $settings = [], $run = true)
+    {
+        
+        self::setPath($settings['path'] ?? __DIR__ . '/../');
+
+        if ($run == true) {
+            self::startLogger();
+        }
+
+        File::createLog();
+    }
+
+
+
+    private static function setPath(string $path)
+    {
+        try {
+
+            if (is_dir($path) && is_writable($path)) {
+
+                define('LOGGER_PATH', $path);
+                
+                self::$path = $path;
+                return true;
+
+            } 
+                
+            throw new LoggerException("[LOGGER] Permission: Please, check the permissions for the path: ${path}");
+
+        } catch (LoggerException $le) {
+            echo $le->getMessage();
+        }
+    }
+
+
+
+}
