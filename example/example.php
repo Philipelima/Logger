fa342b1bc869da7793ed5b913250c836459461d2
Philipelima <philipematheus.lim@gmail.com>
2022-06-29 00:15:32 -0300

    feat: ErrorHandler::setErrorHandler example

--- example/example.php
+++ example/example.php
@@ -3,22 +3,51 @@
 
 require_once '../vendor/autoload.php';
 
+use Logger\Handler\ErrorHandler;
+
 use Logger\Logger;
-use Logger\Settings\Settings;
+use Logger\Settings\LoggerSettings;
 
 /**
  * settings  for the Logger
  * 1 - array
  */
-Settings::settings([
+LoggerSettings::settings([
     'path' => realpath(__DIR__.'/../')
 ]);
 
 
-Logger::info("Hello, this is a test message.");
 
-Logger::warn("Attention, failed login attempt.");
 
-Logger::success("Success registering user.");
+function my_error_handler($errno, $errstr, $errfile, $errline) {
 
-Logger::error("Failed to register user.");
+    if(!( error_reporting() & $errno )) return false;
+    $errstr = htmlspecialchars($errstr);
+
+    switch ($errno) {
+
+        case E_USER_ERROR:
+            Logger::error($errstr);
+            break;
+        
+        case E_USER_WARNING:
+            Logger::warn($errstr);
+            break;
+        
+        case E_USER_NOTICE:
+            Logger::notice($errstr);
+            break;
+
+        default:
+            Logger::info(" undefined error at  {$errfile} ");
+            break;
+    }
+}
+
+
+ErrorHandler::setErrorHandler('my_error_handler');
+
+
+
+
+//Info message
