109f5672b2fbf6876a0769dcc33cec439e514c1d
Philipelima <philipematheus.lim@gmail.com>
2022-06-29 01:02:51 -0300

    feat: ExceptionHandler::setExceptionHandler example

--- example/example.php
+++ example/example.php
@@ -1,53 +1,26 @@
 <?php
-
-
 require_once '../vendor/autoload.php';
 
-use Logger\Handler\ErrorHandler;
-
-use Logger\Logger;
 use Logger\Settings\LoggerSettings;
+use Logger\Handler\ExceptionHandler;
+use Logger\Handler\ErrorHandler;
 
 /**
  * settings  for the Logger
- * 1 - array
  */
 LoggerSettings::settings([
     'path' => realpath(__DIR__.'/../')
 ]);
 
 
+require __DIR__.'/customHandler/customErrorHandler.php';
+require __DIR__.'/customHandler/customExceptionHandler.php';
 
 
-function my_error_handler($errno, $errstr, $errfile, $errline) {
-
-    if(!( error_reporting() & $errno )) return false;
-    $errstr = htmlspecialchars($errstr);
-
-    switch ($errno) {
-
-        case E_USER_ERROR:
-            Logger::error($errstr);
-            break;
-        
-        case E_USER_WARNING:
-            Logger::warn($errstr);
-            break;
-        
-        case E_USER_NOTICE:
-            Logger::notice($errstr);
-            break;
-
-        default:
-            Logger::info(" undefined error at  {$errfile} ");
-            break;
-    }
-}
-
-
-ErrorHandler::setErrorHandler('my_error_handler');
+ExceptionHandler::setExceptionHandler('custom_exception_handler');
+ErrorHandler::setErrorHandler('custom_error_handler');
 
 
 
 
-//Info message
+
